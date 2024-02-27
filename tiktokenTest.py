4a172e5af0c493b485ac97411ffc6c553a9b3f08
jackial <jiangyang118@163.com>
2024-02-27 10:13:05 +0800

    add test
    测试gpt的tiktoken

---
+++ tiktokenTest.py
@@ -0,0 +1,8 @@
+import tiktoken
+
+# enc = tiktoken.get_encoding('gpt3')
+enc = tiktoken.get_encoding('gpt2')
+print(enc.n_vocab)
+
+print(enc.encode('hii there'))
+print(enc.decode([71, 4178, 612]))
